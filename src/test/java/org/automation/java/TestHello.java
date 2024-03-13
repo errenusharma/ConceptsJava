d370f375c86baff7dbe14b8e29f383d457c0d694
erren <errenusharma@gmail.com>
2024-03-12 20:35:32 -0400

    Updated Test

---
+++ src/test/java/org/automation/java/TestHello.java
@@ -0,0 +1,12 @@
+package org.automation.java;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestHello {
+
+    @Test
+    public void testHello(){
+        Assertions.assertTrue(Boolean.TRUE);
+    }
+}
