f1b5f94aad6d7c7386757f6af3c6fe0d761257bb
erren <errenusharma@gmail.com>
2024-03-19 12:37:00 -0400

    Rev1.2

---
+++ src/test/java/org/automation/java/TestDuplicateElements.java
@@ -0,0 +1,23 @@
+package org.automation.java;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TestDuplicateElements {
+
+    @Test
+    public void testDuplicateElements()
+    {
+        String str = "WorkInProgress";
+        ReverseString string = new ReverseString();
+
+        System.out.println("Reversed String using basic string = " + ReverseString.reverseString(str));
+        System.out.println("Reversed String using StringBuffer = " + string.reverseString2(str));
+        System.out.println("Reversed String using StringBuilder = " + string.reverseString3("Java"));
+        //StringBuilder result = string.reverseString3("Java");
+        Assertions.assertEquals("avaJ",string.reverseString3("Java"));
+
+    }
+
+}
---
+++ src/test/java/org/automation/java/TestPyramid.java
@@ -0,0 +1,11 @@
+package org.automation.java;
+
+import org.junit.jupiter.api.Test;
+
+public class TestPyramid {
+    @Test
+    public void testCreatePyramid(){
+        Pyramid p1 = new Pyramid();
+        p1.createPyramid();
+    }
+}
